90d25591fbb14e4e23cfc8b25c494cc5c109d88f
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2024-04-18 09:58:55 +0300

    add home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './routers/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+    // children: []
+  },
+  {
+    path: 'home', // /home
+    // pathMatch: 'prefix', // Default // ^(/home)
+    component: HomePageComponent,
+    // children: [
+    //   {
+    //     path: "models", // /home/models
+    //     // pathMatch: 'prefix', // ^(/home/models)
+    //     component: HomePageComponent,
+    //   }
+    // ]
+  }
+];

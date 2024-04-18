8b4f79314f4a17716ef3cabf82526048d0b82aa0
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2024-04-18 12:01:28 +0300

    add counter, if and getter and setter example

--- src/app/routers/home-page/home-page.component.ts
+++ src/app/routers/home-page/home-page.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MainLayoutComponent } from '../../shared/layouts/main-layout/main-layout.component';
@@ -5,10 +6,42 @@ import { MainLayoutComponent } from '../../shared/layouts/main-layout/main-layou
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [RouterModule, MainLayoutComponent],
+  imports: [CommonModule, RouterModule, MainLayoutComponent],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrl: './home-page.component.scss',
 })
 export class HomePageComponent {
+  private _counter: number = 0;
+  get counter() : number {
+    console.log('Counter get edildi');
+    return this._counter;
+  }
+  set counter(value: number) {
+    console.log('Counter set edildi', value);
+    this._counter = value;
+  }
+  // getCounter() { // Metot
+  //   return this.counter;
+  // }
+  // setCounter(value: number) { // Metot
+  //   this.counter = value;
+  // }
 
+  sumCounter(sum: number) { // Metot
+    this.counter = this.counter + sum;
+  }
+
+  // Getter
+  get isCounterZero() : boolean {
+    return this.counter === 0;
+  }
+  getIsCounterPositive() : boolean { // Metot
+    return this.counter > 0;
+  }
+  // get isCounterPositive() : boolean {
+  //   return this.counter > 0;
+  // }
+  get isCounterNegative() : boolean {
+    return this.counter < 0;
+  }
 }

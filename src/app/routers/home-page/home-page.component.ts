e2d0759f9b825f5da80cec0651da5d34f85a8d50
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2024-04-18 10:54:15 +0300

    add main layout component

--- src/app/routers/home-page/home-page.component.ts
+++ src/app/routers/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MainLayoutComponent } from '../../shared/layouts/main-layout/main-layout.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MainLayoutComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
